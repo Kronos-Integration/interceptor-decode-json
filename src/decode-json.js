94156623478658615e19ad995a69c8d37c606c5c
Markus Felten <Markus.Felten@gmx.de>
2017-10-17 23:13:21 +0200

    style: mode export attribute to object defintion

--- src/decode-json.js
+++ src/decode-json.js
@@ -1,42 +1,32 @@
-/* jslint node: true, esnext: true */
-
-'use strict';
-
 const pcs = require('parse-concat-stream');
 
-import {
-	Interceptor
-}
-from 'kronos-interceptor';
+import { Interceptor } from 'kronos-interceptor';
 
 /**
  *
  */
-class DecodeJSONInterceptor extends Interceptor {
-	static get name() {
-		return 'decode-json';
-	}
+export class DecodeJSONInterceptor extends Interceptor {
+  static get name() {
+    return 'decode-json';
+  }
 
-	receive(request, args) {
-		return new Promise((fullfilled, rejected) =>
-			request.payload.pipe(pcs((err, data) => {
-				if (err) {
-					rejected(err);
-				} else {
-					fullfilled({
-						data: data
-					});
-				}
-			})));
-	}
+  receive(request, args) {
+    return new Promise((fullfilled, rejected) =>
+      request.payload.pipe(
+        pcs((err, data) => {
+          if (err) {
+            rejected(err);
+          } else {
+            fullfilled({
+              data: data
+            });
+          }
+        })
+      )
+    );
+  }
 }
 
-
-function registerWithManager(manager) {
-	manager.registerInterceptor(DecodeJSONInterceptor);
+export function registerWithManager(manager) {
+  manager.registerInterceptor(DecodeJSONInterceptor);
 }
-
-export {
-	DecodeJSONInterceptor,
-	registerWithManager
-};
