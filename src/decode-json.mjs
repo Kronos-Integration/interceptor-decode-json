145a9a100ad9c1087908516223617679da833671
Markus Felten <Markus.Felten@gmx.de>
2019-12-23 15:46:50 +0100

    feat: encode json

--- src/decode-json.mjs
+++ src/decode-json.mjs
@@ -14,5 +14,20 @@ export class DecodeJSONInterceptor extends Interceptor {
   }
 }
 
+/**
+ *
+ */
+export class EncodeJSONInterceptor extends Interceptor {
+  static get name() {
+    return 'encode-json';
+  }
 
+  async receive(endpoint, next, request) {
+    return JSON.parse(await next(JSON.stringify(request)));
+  }
+}
+
+export default EncodeJSONInterceptor;
 export default DecodeJSONInterceptor;
+
+
