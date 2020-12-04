48bcb5f4e8c384d4456b3a31065722e77e7de273
Markus Felten <Markus.Felten@gmx.de>
2020-12-04 23:05:26 +0100

    add missing src/index.mjs from template

---
+++ src/index.mjs
@@ -0,0 +1,6 @@
+export * from "./interceptor.mjs";
+export * from "./timeout-interceptor.mjs";
+export * from "./stats-collector-interceptor.mjs";
+export * from "./limiting-interceptor.mjs";
+export * from "./logging-interceptor.mjs";
+export * from "./template-interceptor.mjs";
