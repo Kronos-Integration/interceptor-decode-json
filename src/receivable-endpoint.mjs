84b2768e63265cb4627cdee32f2831c01f6b3514
Markus Felten <Markus.Felten@gmx.de>
2020-12-04 23:04:53 +0100

    add missing src/receivable-endpoint.mjs from template

---
+++ src/receivable-endpoint.mjs
@@ -0,0 +1,57 @@
+import { Endpoint, instanciateInterceptors } from "./endpoint.mjs";
+
+const RECEIVE = Symbol("receive");
+
+/**
+ * @param {string} name endpoint name
+ * @param {Object} owner of the endpoint (service)
+ * @param {Object} options
+ * @param {Function} [options.receive] reciever function
+ * @param {Function} [options.receivingInterceptors]
+ */
+export class ReceivableEndpoint extends Endpoint {
+  constructor(name, owner, options) {
+    super(name, owner, options);
+    if (options.receive) {
+      this.receive = options.receive;
+    }
+
+    if (options.receivingInterceptors) {
+      Object.defineProperties(this, {
+        receivingInterceptors: {
+          value: instanciateInterceptors(
+            options.receivingInterceptors,
+            this.owner
+          )
+        }
+      });
+    }
+  }
+
+  /**
+   * @return {boolean} true is receive function is present
+   */
+  get isIn() {
+    return this[RECEIVE] !== undefined;
+  }
+
+  /**
+   * Get the receive function.
+   * @return {Function}
+   */
+  get receive() {
+    return this[RECEIVE];
+  }
+
+  /**
+   * Set the receive function.
+   * @param {Function} receive
+   */
+  set receive(receive) {
+    this[RECEIVE] = receive;
+  }
+
+  get isOpen() {
+    return this[RECEIVE] !== undefined;
+  }
+}
